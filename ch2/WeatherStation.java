da86a787b3c02f68320244d5314b4ffc3354697f
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-10-30 15:35:50 +0200

    Added WeatherStation 1.0

---
+++ ch2/WeatherStation.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+
+        weatherData.setMeasurements(132, 312, 32.4f);
+        weatherData.setMeasurements(134, 311, 31.4f);
+        weatherData.setMeasurements(133, 310, 33.4f);
+    }
+}
