2e6eedb2ab3ccb13613fa74a27d3ed561d474c56
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-10-31 13:41:50 +0200

    Added heatidx calc 1.0

--- ch2/WeatherData.java
+++ ch2/WeatherData.java
@@ -8,6 +8,7 @@ public class WeatherData implements Subject {
     private float temp;
     private float humidity;
     private float pressure;
+    private float heatIndex;
 
     public WeatherData() {
         observers = new ArrayList<Observer>();
@@ -23,7 +24,7 @@ public class WeatherData implements Subject {
 
     public void notifyObservers() {
         for (Observer observer : observers) {
-            observer.update(this.temp, this.humidity, this.pressure);
+            observer.update(this.temp, this.humidity, this.pressure, this.heatIndex);
         }
     }
 
@@ -35,7 +36,21 @@ public class WeatherData implements Subject {
         this.temp = temp;
         this.humidity = humidity;
         this.pressure = pressure;
+        this.heatIndex = computeHeatIndex(temp, humidity);
 
         measurementsChanged();
+    }
+
+    private float computeHeatIndex(float t, float rh) {
+        float index = (float) ((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) +
+                (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) +
+                (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+                (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+                        (rh * rh * rh))
+                + (0.00000142721 * (t * t * t * rh)) +
+                (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+                0.000000000843296 * (t * t * rh * rh * rh)) -
+                (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+        return index;
     }
 }
