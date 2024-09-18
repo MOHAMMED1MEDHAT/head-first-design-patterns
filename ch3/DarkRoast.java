85195879c0e6232de1843254c907318e71f285ba
MOHAMMED1MEDHAT <mohamed.medhat2121@gmail.com>
2024-09-18 11:50:33 +0300

    chore! add formatting & some use case

--- ch3/DarkRoast.java
+++ ch3/DarkRoast.java
@@ -1,11 +1,13 @@
 package ch3;
 
 public class DarkRoast extends Beverage {
+
     public DarkRoast() {
         this.description = "DarkRoast";
     }
 
     @Override
+
     public double cost() {
         return .99;
     }
--- ch3/StarBuzzCoffee.java
+++ ch3/StarBuzzCoffee.java
@@ -3,34 +3,32 @@ package ch3;
 import ch3.Beverage.Size;
 
 public class StarBuzzCoffee {
-        public static void main(String[] args) {
-                Beverage espresso = new Espresso();
-                espresso.setSize(Size.GRANDE);
-                System.out.println(espresso.getDescription()
-                                + " $" + espresso.cost());
 
-                Beverage houseBlend = new HouseBlend();
-                houseBlend = new Mocha(houseBlend);
-                houseBlend = new Mocha(houseBlend);
-                houseBlend = new Mocha(houseBlend);
-                houseBlend.setSize(Size.GRANDE);
-                System.out.println(houseBlend.getDescription()
-                                + " $" + houseBlend.cost());
+    public static void main(String[] args) {
+        Beverage espresso = new Espresso();
+        espresso.setSize(Size.GRANDE);
+        System.out.println(espresso.getDescription() + " $" + espresso.cost());
 
-                Beverage decaf = new Decaf();
-                decaf = new Mocha(decaf);
-                decaf = new Soy(decaf);
-                decaf = new SteamedMilk(decaf);
-                decaf.setSize(Size.GRANDE);
-                System.out.println(decaf.getDescription()
-                                + " $" + decaf.cost());
+        Beverage houseBlend = new HouseBlend();
+        houseBlend = new Mocha(houseBlend);
+        houseBlend = new Mocha(houseBlend);
+        houseBlend = new Mocha(houseBlend);
+        houseBlend.setSize(Size.GRANDE);
+        System.out.println(houseBlend.getDescription() + " $" + houseBlend.cost());
 
-                Beverage darkRoast = new DarkRoast();
-                darkRoast = new Mocha(darkRoast);
-                darkRoast = new Soy(darkRoast);
-                darkRoast.setSize(Size.GRANDE);
-                System.out.println(darkRoast.getDescription()
-                                + " $" + darkRoast.cost());
+        Beverage decaf = new Decaf();
+        decaf = new Mocha(decaf);
+        decaf = new Soy(decaf);
+        decaf = new SteamedMilk(decaf);
+        decaf.setSize(Size.GRANDE);
 
-        }
+        System.out.println(decaf.getDescription() + " $" + decaf.cost());
+
+        Beverage darkRoast = new DarkRoast();
+        darkRoast = new Mocha(darkRoast);
+        darkRoast = new Soy(darkRoast);
+        darkRoast.setSize(Size.GRANDE);
+        System.out.println(darkRoast.getDescription() + " $" + darkRoast.cost());
+
+    }
 }
